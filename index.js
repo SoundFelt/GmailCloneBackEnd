513dd6429bd474b2a26edcdadc7362223713b9a9
Anthony McLean <anthony@192.168.1.104>
2022-04-19 18:34:12 +0100

    work

--- index.js
+++ index.js
@@ -9,14 +9,10 @@ app.use(cors({ origin: true }));
 
 
 const connection = mysql.createConnection({
-    // host: 'eu-cdbr-west-02.cleardb.net',
-    // user: 'b387195cb48ad4',
-    // password: '6b12f8b0',
-    // database: 'heroku_0a9b6022d153e93'
-    host: 'localhost',
-    user: 'root',
-    password: 'Glasscha1r',
-    database: 'gmailclone'
+    host: 'eu-cdbr-west-02.cleardb.net',
+    user: 'b387195cb48ad4',
+    password: '6b12f8b0',
+    database: 'heroku_0a9b6022d153e93'
 })
 
 app.post('/', (req, res) => {
